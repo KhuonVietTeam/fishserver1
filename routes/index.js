8339133e1597f03b17dceb1b8912b57563dd07d8
AnhQuangCee <duonganhquang97@gmail.com>
2018-11-07 15:36:19 +0700

    Setup MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+//Setup MongoDB
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017/contact';
+
+// Database Name
+const dbName = 'contact';
+
+// Use connect method to connect to the server
+MongoClient.connect(url,{ useNewUrlParser: true }, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
