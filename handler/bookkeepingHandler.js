540bfcb36cc7a6e170d6f4d6ba19d18b40318ee8
tuannhanle <tuannhanle@gmail.com>
2018-11-14 10:11:24 +0700

    c1

--- handler/bookkeepingHandler.js
+++ handler/bookkeepingHandler.js
@@ -2,8 +2,8 @@ module.exports = {
     handleCode:async (data)=>{ // xử lý password
         //handle code
         let code =await data.totallyProfits + data.thisTimeNetProfits + data.machineNo + data.enteringCodeTimes + data.checkCode;
-        
-        
+
+
         //return
         return code;
     },
