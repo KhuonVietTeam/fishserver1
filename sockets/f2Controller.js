98826915b6541e466ce4c108ba99153892777ba5
tuannhanle <tuannhanle@gmail.com>
2018-11-20 15:47:18 +0700

    add-socket 1

--- sockets/f2Controller.js
+++ sockets/f2Controller.js
@@ -4,6 +4,20 @@ var router = app.Router();
 module.exports = function(io) {
     io.on('connection', function(socket) {
         console.log("CONNECTION2");
+
+
+        socket.on('PlayersScore',(msg)=>{
+          console.log(msg);
+          socket.broadcast.emit('PlayersScore',msg);
+          // io.local.emit('Dat_PlayersScore',{"data":9999,"id":1})
+        })
+
+        socket.on('PlayersScore_Change',(msg)=>{
+          console.log(msg);
+          socket.broadcast.emit('PlayersScore_Change',msg);
+
+        })
+
         socket.on('disconnect',function() {
           console.log("DISCONNECTION2");
         })
